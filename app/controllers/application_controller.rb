8abd6ca8aba8beb5e4e99512b0103c30dd948818
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-02-18 18:31:29 -0600

    added static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello, world!"
+  def home
   end
+
+  def help
+  end
+
+  def about
+  end	
+
 end
