685b58a314ac1b584fcce25544e326891453365d
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-03-04 19:42:16 -0600

    adding validation tests to models

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-<<<<<<< HEAD
 	 # Returns the full title on a per-page basis.
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -7,14 +6,6 @@ module ApplicationHelper
       base_title
     else
       page_title + " | " + base_title
-=======
-	def full_title(page_title = '')
-      base_title = "Ruby on Rails Tutorial Sample App"
-      if page_title.empty?
-        base_title
-      else
-        page_title + " | " + base_title
->>>>>>> filling-in-layout
     end
   end
 end
